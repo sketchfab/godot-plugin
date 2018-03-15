14e934d81236e638418c61eeade3809f8e298c3c
Pedro J. Estébanez <pedrojrulez@gmail.com>
2018-03-15 21:28:33 +0100

    Complete result item

--- ResultItem.gd
+++ ResultItem.gd
@@ -2,15 +2,29 @@ extends Container
 
 const SafeData = preload("res://SafeData.gd")
 
+onready var user_name = find_node("UserName")
+onready var model_name = find_node("ModelName")
+onready var image = find_node("Image")
+
 var data
-var viewer_url
 
 func set_data(data):
 	self.data = data
 
 func _ready():
-	$Label.text = (
-		SafeData.string(data, "name") + "\n" +
-		SafeData.string(data, "publishedAt") + "\n"
-	)
-	viewer_url = SafeData.string(data, "viewerUrl")
+	model_name.text = SafeData.string(data, "name")
+	
+	var user = SafeData.dictionary(data, "user")
+	user_name.text = "by %s" % SafeData.string(user, "displayName")
+	
+	var thumbnails = SafeData.dictionary(data, "thumbnails")
+	var images = SafeData.array(thumbnails, "images")
+	
+	var smallest_size = 10e20
+	var smallest_url
+	for img in images:
+		var size = SafeData.integer(img, "width") * SafeData.integer(img, "height")
+		if size < smallest_size:
+			smallest_size = size
+			smallest_url = SafeData.string(img, "url")
+	self.image.url = smallest_url
